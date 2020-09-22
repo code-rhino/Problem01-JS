3d1155ec4a959400310755995c8a980b5c7b0d74
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2020-09-22 14:23:21 -0400

    Updates

--- WuTangForeverBuzz.js
+++ WuTangForeverBuzz.js
@@ -1,13 +1,7 @@
 class WuTangForeverBuzz {
     wuTangClan(inputValue){
         let output ="";
-        for (var i=1; i <= inputValue; i++){
-            if (i % 15 == 0) output +="WuTangForever\n";
-            else if (i % 3 == 0) output +="Wu\n";
-            else if (i % 5 == 0) output +="Tang\n";
-            else output += i+"\n";
-        }
-        console.log(output);
+
         return output;
     }
 }
